35f890800dda709ac9b04e92956ce972d5563a31
BZakro <bennyzakro@gmail.com>
2021-05-06 22:53:37 +0300

    after MailToNote, erased Copy

--- App.jsx
+++ App.jsx
@@ -3,8 +3,7 @@ const { Route, Switch } = ReactRouterDOM
 import { AppHeader } from './cmps/AppHeader.jsx'
 import { HomePage } from './pages/HomePage.jsx'
 import { EmailApp } from './apps/Mail/pages/EmailApp.jsx'
-// import { NoteApp } from './apps/Keep/pages/NoteApp.jsx'
-import { NoteAppCopy } from './apps/Keep/pages/NoteAppCopy.jsx'
+import { NoteApp } from './apps/Keep/pages/NoteApp.jsx'
 import { BookApp } from './apps/Books/pages/BookApp.jsx'
 import { AboutUs } from './pages/AboutUs.jsx'
 
@@ -20,8 +19,7 @@ export function App() {
                     <Switch>
                         <Route component={BookApp} path="/book" />
                         <Route component={EmailApp} path="/email" />
-                        {/* <Route component={NoteApp} path="/keep" /> */}
-                        <Route component={NoteAppCopy} path="/keep" />
+                        <Route component={NoteApp} path="/keep" />
                         <Route component={AboutUs} path="/about" />
                         <Route component={HomePage} path="/" />
                     </Switch>
