c0a3c20af2ed921230e31858a74befd4fb947f29
BZakro <bennyzakro@gmail.com>
2021-05-09 00:33:15 +0300

    done for now

--- pages/AboutUs.jsx
+++ pages/AboutUs.jsx
@@ -3,13 +3,13 @@ export function AboutUs(props) {
   return <section className="about-container flex">
 
     <div className="ben column align-center">
-      <img src="https://robohash.org/dfhg" alt="" />
+      <img src="https://bzakro.github.io/gallery-proj-try3/img/team/2.jpg" />
       <h2>Ben</h2>
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis alias cumque fugit, doloribus provident quis sint ducimus earum eos quod aspernatur beatae molestiae illo minima, voluptate deleniti odit libero sequi!</p>
+      <p>Hey i'm Benny, my friends call me Ben. Ever since i started this course i am dreaming about code. It kinda freaks me out but i enjoy it overall. I hope to quickly get into the programming world and establish myself as a successful full-stack developer. </p>
     </div>
 
     <div className="ilya column align-center">
-      <img src="https://robohash.org/dsfhg" alt="" />
+      <img src="https://robohash.org/dsfhg"  />
       <h2>Ilya</h2>
       <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis alias cumque fugit, doloribus provident quis sint ducimus earum eos quod aspernatur beatae molestiae illo minima, voluptate deleniti odit libero sequi!</p>
     </div>
