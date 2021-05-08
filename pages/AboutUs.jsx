a4f4d8c85839700935b1d68391df1f9874dbff67
IlyaKad <iliya98@gmail.com>
2021-05-09 00:33:51 +0300

    Night

--- pages/AboutUs.jsx
+++ pages/AboutUs.jsx
@@ -3,19 +3,16 @@ export function AboutUs(props) {
   return <section className="about-container flex">
 
     <div className="ben column align-center">
-      <img src="https://robohash.org/dfhg" alt="" />
+      <img src="https://robohash.org/dfhg"/>
       <h2>Ben</h2>
       <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis alias cumque fugit, doloribus provident quis sint ducimus earum eos quod aspernatur beatae molestiae illo minima, voluptate deleniti odit libero sequi!</p>
     </div>
 
     <div className="ilya column align-center">
-      <img src="https://robohash.org/dsfhg" alt="" />
+      <img src="assets/app.img/Ilya.jpg" />
       <h2>Ilya</h2>
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis alias cumque fugit, doloribus provident quis sint ducimus earum eos quod aspernatur beatae molestiae illo minima, voluptate deleniti odit libero sequi!</p>
+      <p>Hi my name is Ilya, but my friend call me "Lorem". Because i`m working in "Ipsom" corp. Just joking, I was a Mechanical Engineer but nowadays I am a Fullstack developer.</p>
     </div>
-
-
-
 
   </section>
 }
